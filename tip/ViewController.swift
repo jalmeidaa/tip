cfd7ec2d50026d9f62cec8625aa4e1deb2bc4bae
Sarah Zhou <syzhou@fb.com>
2016-06-14 14:50:21 -0700

    test

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var percentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
-
+    @IBAction func calcTip(sender: AnyObject) {
+        
+        let tipPercent = [0.15, 0.18, 0.2]
+        
+        let bill = Double(billField.text!) ?? 0
+        // "?? 0" returns nil if the input is not a number
+        let tip = bill * tipPercent[percentControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
